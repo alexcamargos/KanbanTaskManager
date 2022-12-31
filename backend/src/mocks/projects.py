817ec0e515cf451e64a6713c83a46a39e1c36735
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-31 13:45:43 -0300

    feat: add mocks for projects
        This file contains the mock for the projects model.
        It is used to test the projects routes.

---
+++ backend/src/mocks/projects.py
@@ -0,0 +1,41 @@
+from typing import NamedTuple
+import datetime
+
+
+class Project(NamedTuple):
+    id: int
+    name: str
+    details: str
+    status: str
+    creation_date: datetime.date
+    alteration_date: datetime.date
+    tasks_ids: list
+
+
+_project_01 = Project(id=1,
+                      name='Project 01',
+                      details='Detailed description of project 01.',
+                      status='ToDo',
+                      creation_date=datetime.date(2022, 12, 15),
+                      alteration_date=datetime.date(2022, 12, 20),
+                      tasks_ids=[1, 2, 7, 8])
+
+_project_02 = Project(id=2,
+                      name='Project 02',
+                      details='Detailed description of project 02.',
+                      status='ToDo',
+                      creation_date=datetime.date(2022, 12, 15),
+                      alteration_date=datetime.date(2022, 12, 20),
+                      tasks_ids=[3, 4, 9, 10])
+
+_project_03 = Project(id=3,
+                      name='Project 03',
+                      details='Detailed description of project 03.',
+                      status='done',
+                      creation_date=datetime.date(2022, 12, 15),
+                      alteration_date=datetime.date(2022, 12, 16),
+                      tasks_ids=[5, 6])
+
+PROJECT_LIST = [_project_01, _project_02, _project_03]
+
+__all__ = ['PROJECT_LIST']
