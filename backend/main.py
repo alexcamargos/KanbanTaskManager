eeccb92f32ddaedba9c1563f1597796f54018bd0
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2023-01-01 07:11:09 -0300

    feat: add tasks api interface
        This interface implement the tasks routes.

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from src.routers import projects
+from src.routers import tasks
 
 app = FastAPI()
 
 app.include_router(projects.router)
+app.include_router(tasks.router)
 
 
 @app.get('/')
