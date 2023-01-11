a650330e8761a13bba546e70169d1e35c9938e15
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2023-01-11 06:34:19 -0300

    refactor: move app-root to components folder

--- frontend/src/app/components/app-root/app-root.component.spec.ts
+++ frontend/src/app/components/app-root/app-root.component.spec.ts
@@ -1,16 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-root.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  it(`should have as title 'Kanban Task Manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.title).toEqual('Kanban Task Manager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'frontend app is running!'
+    );
   });
 });
---
+++ frontend/src/app/components/app-root/app-root.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app-root.component.html',
+  styleUrls: ['./app-root.component.sass'],
+})
+export class AppComponent {
+  title = 'Kanban Task Manager';
+}
