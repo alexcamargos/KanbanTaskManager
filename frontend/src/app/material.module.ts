84910a2171892dfe27736779a17cd2fe936508ee
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2023-01-11 06:30:59 -0300

    feat: add Angular Material
      Material Design components for Angular.

---
+++ frontend/src/app/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+
+@NgModule({
+  exports: [MatCardModule, MatIconModule, MatButtonModule, MatDividerModule],
+})
+export class AngularMaterialModule {}
