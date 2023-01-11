c0393aaee460eb6618be18e3a95a9e71ae047003
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2023-01-11 06:35:38 -0300

    feat: add KanbanTaskManagerService
        This service is responsible for get data for backend.

--- frontend/src/app/services/kanban-task-manager.service.spec.ts
+++ frontend/src/app/services/kanban-task-manager.service.spec.ts
@@ -1,12 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
 import { KanbanTaskManagerService } from './kanban-task-manager.service';
 
 describe('KanbanTaskManagerService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let service: KanbanTaskManagerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
     service = TestBed.inject(KanbanTaskManagerService);
   });
 
--- frontend/src/app/services/kanban-task-manager.service.ts
+++ frontend/src/app/services/kanban-task-manager.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+import { Task } from '../models/task.model';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class KanbanTaskManagerService {
+  private apiUrl: string;
+  private tasks: Task | any;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl;
+  }
+
+  getAllTasks(): Observable<Task> {
+    return this.http.get<Task>(`${this.apiUrl}/tasks`);
+  }
+
+  getTaskById(id: number): Observable<Task> {
+    return this.http.get<Task>(`${this.apiUrl}/tasks/${id}`);
+  }
 }
