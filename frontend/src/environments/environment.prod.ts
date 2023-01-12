6012c5b0de9538c5e531f379ae59ae591b229584
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2023-01-12 06:42:06 -0300

    config: add environment for production
        In production mode the environment apiUrl is set to render.com

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+let baseURL: string = 'https://kanban-task-manager.onrender.com';
+let baseEndPoint: string = '/api/v1';
+
+export const environment = {
+  production: true,
+  apiUrl: `${baseURL}${baseEndPoint}`,
+};
