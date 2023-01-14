5ee96a55c401efe9f8abddb6001d18ae5e6a98e2
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2023-01-14 07:05:56 -0300

    feat: add CORS support
        Request from the frontend are now allowed
        to be made from any origin.

--- backend/main.py
+++ backend/main.py
@@ -13,16 +13,27 @@
 #
 #  License: MIT
 #  ------------------------------------------------------------------------------
-
 """Kanban Task Manager.
 
 This is a simple Task Manager that makes it easy for you to keep track of all Tasks and To Dos.
 """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.api.v1.routers import projects, tasks
 
 app = FastAPI()
+
+ORIGINS = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(projects.router, prefix='/api/v1', tags=["projects"])
 app.include_router(tasks.router, prefix='/api/v1', tags=["tasks"])
