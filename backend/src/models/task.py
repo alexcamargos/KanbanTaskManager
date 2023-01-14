8ec8abfee6417f71bb11683f04b12b25c1bc6b94
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2023-01-14 13:49:58 -0300

    feat: add model for tasks

---
+++ backend/src/models/task.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+#  ------------------------------------------------------------------------------
+#  Name: tasks.py
+#  Version: 0.0.1
+#  Summary: Kanban Task Manager
+#           This is a simple TaskResponse Manager that makes it easy for you to keep
+#           track of all Tasks and To Dos.
+#
+#  Author: Alexsander Lopes Camargos
+#  Author-email: alcamargos@vivaldi.net
+#
+#  License: MIT
+#  ------------------------------------------------------------------------------
+
+from datetime import datetime
+
+from pydantic.main import BaseModel
+
+
+class TaskModel(BaseModel):
+    id: int
+    name: str
+    details: str
+    status: int
+    priority: int
+    creation_date: datetime
+    alteration_date: datetime
+    project_id: int
