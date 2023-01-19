8c42fdbe1017730471af08d7e0434237eaef0337
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2023-01-19 18:34:32 -0300

    feat: add projects schema
        ProjectResponse: A single project response.
        MultipleProjectResponse: A multiple project response.

--- backend/src/schemas/project.py
+++ backend/src/schemas/project.py
@@ -13,21 +13,40 @@
 #
 #  License: MIT
 #  ------------------------------------------------------------------------------
-
 """Kanban Task Manager.
 
 This is a simple Task Manager that makes it easy for you to keep track of all Tasks and To Dos.
 """
 
-import datetime
-from typing import NamedTuple
+from datetime import datetime
+
+from pydantic.main import BaseModel
+
+from ..models.project import ProjectModel
 
 
-class Project(NamedTuple):
+class ProjectResponse(BaseModel):
     id: int
     name: str
     details: str
     status: int
-    creation_date: datetime.date
-    alteration_date: datetime.date
+    creation_date: datetime
+    alteration_date: datetime
     tasks_ids: list
+
+    @classmethod
+    def from_project_instance(cls, project: ProjectModel) -> "ProjectResponse":
+        return cls(**project.dict())
+
+
+class MultipleProjectResponse(BaseModel):
+    projects: list[ProjectModel]
+    projects_count: int
+
+    @classmethod
+    def from_multiple_project_instance(
+            cls,
+            projects: list[ProjectModel],
+            projects_count: int) -> "MultipleProjectResponse":
+        return cls(projects=[project for project in projects],
+                   projects_count=projects_count)
