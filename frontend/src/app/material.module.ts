d84ad33fb6f80ea67dcec4c41e7a7ea53e643a4b
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2023-01-25 06:01:21 -0300

    feat: import MatToolbarModule from @angular/material/toolbar

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-  exports: [MatCardModule, MatIconModule, MatButtonModule, MatDividerModule],
+  exports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
 })
 export class AngularMaterialModule {}
