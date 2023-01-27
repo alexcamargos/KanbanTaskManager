84c1d79d75f84f3d449201292e504144d9edbc1a
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2023-01-27 06:46:32 -0300

    feat: import @angular/cdk/drag-drop

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -5,6 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   exports: [
@@ -13,6 +14,7 @@ import { MatDividerModule } from '@angular/material/divider';
     MatIconModule,
     MatButtonModule,
     MatDividerModule,
+    DragDropModule,
   ],
 })
 export class AngularMaterialModule {}
